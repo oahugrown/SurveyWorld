da230e2cd5f03b1fbad384cf78f7061aec937fe4
yvy808 <yvonnemyeung@gmail.com>
2017-06-12 22:03:12 -0700

    Reorganized assets
    - moved stuff
    - created intro sceen
    - touched up canvas looks for multiple devices
    - added scenes to build settings

---
+++ Unity/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour
+{
+
+	public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
